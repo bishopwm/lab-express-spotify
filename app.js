8ff48fab13992ac9a15be11c743bcb4e8931488b
bishopwm <bishopwm@g.cofc.edu>
2020-05-12 14:57:02 -0400

    completed through iteration 4

--- app.js
+++ app.js
@@ -28,3 +28,55 @@ const spotifyApi = new SpotifyWebApi({
 // Our routes go here:
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+app.get("/", (req, res) => {
+    res.render('home.hbs');
+  });
+
+app.get("/artist-search", (req, res) => {
+    console.log(req.query)
+    spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+      console.log(data.body.artists.items[0])
+      let artistName=data.body.artists.items[0].name;
+      let artistImage=data.body.artists.items[0].images[0].url
+      let artistUrl=data.body.artists.items[0].href
+      let artistId=data.body.artists.items[0].id
+      
+      res.render('artist-search-results.hbs', {artistName, artistImage, artistUrl, artistId})
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+
+app.get("/albums/:artistId", (req, res) => {
+
+    spotifyApi.getArtistAlbums(req.params.artistId).then(
+        function(data) {
+          console.log('Artist albums', data.body);
+
+          let albums=data.body.items;
+        
+          res.render('albums.hbs', {albums} )
+        },
+        function(err) {
+          console.error(err);
+        }
+      );
+
+   
+})
+app.get("/tracks/:albumId", (req, res) => {
+    spotifyApi.getAlbumTracks(req.params.albumId, { limit : 5, offset : 1 })
+        .then(function(data) {
+            console.log(data.body);
+
+            let tracks = data.body.items
+
+            res.render('tracks.hbs', {tracks})
+        }, function(err) {
+            console.log('Something went wrong!', err);
+        });
+
+    
+})
